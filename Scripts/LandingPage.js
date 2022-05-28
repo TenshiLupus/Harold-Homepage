86bab3271c8e30f15913f49658a4da1f01499c30
Angel Cardenas Martinez <TenshiLupus@hotmail.com>
2022-05-28 16:39:42 +0200

    Section images added and styled with css

--- Scripts/LandingPage.js
+++ Scripts/LandingPage.js
@@ -1,4 +1,16 @@
-const services = "Portrait - Weddings - Events";
 
-const marqueeText = new Array(50).fill(services);
-console.log("all working");
+const elongateMarquee = () => {
+
+    const services = 'Portrait - Weddings - Events';
+
+    const marqueeText = new Array(50).fill(services).join(' — ');
+    console.log
+
+    const marquee = document.querySelector('.marquee span');
+    console.log(marquee);
+    marquee.textContent = marqueeText;
+    console.log("all working");
+
+}
+
+elongateMarquee();
